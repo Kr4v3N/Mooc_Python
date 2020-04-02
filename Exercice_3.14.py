a5b53953ccca28d28b596fb0a461cb90f5a604f9
Kr4v3n <fafachen37@gmail.com>
2020-04-02 02:28:59 +0200

    Exercice UpyLaB 3.14

---
+++ Exercice_3.14.py
@@ -0,0 +1,32 @@
+"""
+Auteur:Fayçal Chena
+Date : 02 avril 2020
+Consignes :
+Écrire un programme qui génère de manière (pseudo) aléatoire un entier (nombre secret)
+compris entre 0 et 100. Ensuite, le joueur doit deviner ce nombre en utilisant
+le moins d’essais possible.
+"""
+
+import random
+
+NB_ESSAIS_MAX = 6
+secret = random.randint(0, 100)
+print("le nombre secret est", secret)
+QUESTION = "Deviner la valeur choisie entre 0 et 100: "
+essais = 0
+
+n = int(input(QUESTION))
+essais = 1
+
+while n != secret:
+    if essais < 6:
+        if n < secret:
+            print('Trop petit')
+        else:
+            print('Trop grand')
+        essais += 1
+        n = int(input())
+    else:
+        print('Perdu ! le secret était', secret)
+        exit()
+print('Gagné en', essais, 'essais !')
