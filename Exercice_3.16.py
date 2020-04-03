11bddfa793f301d1f0d33bbd3f48663f769ddbd2
Kr4v3n <fafachen37@gmail.com>
2020-04-03 08:03:51 +0200

    Exercice UpyLaB 3.16

---
+++ Exercice_3.16.py
@@ -0,0 +1,22 @@
+"""
+Auteur:Fayçal Chena
+Date : 03 avril 2020
+Consignes :
+Écrivez un code qui lit un nombre entier strictement positif n et affiche sur n lignes
+une table de multiplication de taille  n x n, avec, pour i entre 1 et n,
+les n premières valeurs multiples de i strictement positives sur la ième ligne.
+Ainsi, les n premiers multiples de 1 strictement positifs (0 non compris)
+sont affichés sur la première ligne, les n premiers multiples de 2 sur la deuxième,
+et cætera.
+"""
+n = int(input("Donner votre valeur : \n"))
+
+ligne1 = 1
+
+for i in range(n):
+    ligne2 = 1
+    for j in range(n):
+        print(ligne1 * ligne2, end=" ")
+        ligne2 = ligne2 + 1
+    ligne1 = ligne1 + 1
+    print()
